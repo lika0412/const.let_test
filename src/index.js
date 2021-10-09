0576c93bb1458ee7b2402a2a5d2fcc212f133d6e
lika0412 <92198635+lika0412@users.noreply.github.com>
2021-10-09 15:34:19 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+/**
+ * const,letの変数宣言 
+ / */
+
+/**
+ * let構文
+ *
+ */
+/**
+ * constの変数宣言
+ */
+
+//  const const3 = "const declaration";
+// console.log(const3);
+
+// const val4 = {
+//   name: "MARCO",
+//   age: 46
+// };
+
+// val4.name = "TATH";
+// val4.address = "wal.whole";
+// console.log(val4);
+
+//constで定義したプロパティには変換が可能
+const val5 = ["dog", "cat"];
+val5[0] = "bird";
+val5.push("monkey");
+console.log(val5);
